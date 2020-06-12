8927b84d20fcc9fba9282930fd45444f6adfb96f
mmamoru <mmm.mamoru@toki.waseda.jp>
2020-06-12 18:07:56 +0900

    save

---
+++ eval_ja_seq2seq.py
@@ -0,0 +1,251 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import os
+import torch
+import torch.nn as nn
+from Voc import *
+from model import *
+
+device = torch.device("cuda:0" if torch.cuda.is_available else "cpu")
+
+
+class GreedySearchDecoder(nn.Module):
+    def __init__(self, encoder, decoder):
+        super(GreedySearchDecoder, self).__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+
+    def forward(self, input_seq, input_length, max_length):
+        # Forward input through encoder model
+        encoder_outputs, encoder_hidden = self.encoder(input_seq, input_length)
+        # Prepare encoder's final hidden layer to be first hidden input to the decoder
+        decoder_hidden = encoder_hidden[: self.decoder.n_layers]
+        # Initialize decoder input with SOS_token
+        decoder_input = torch.ones(1, 1, device=device, dtype=torch.long) * SOS_token
+        # Initialize tensors to append decoded words to
+        all_tokens = torch.zeros([0], device=device, dtype=torch.long)
+        all_scores = torch.zeros([0], device=device)
+        # Iteratively decode one word token at a time
+        for _ in range(max_length):
+            # Forward pass through decoder
+            decoder_output, decoder_hidden = self.decoder(
+                decoder_input, decoder_hidden, encoder_outputs
+            )
+            # Obtain most likely word token and its softmax score
+            decoder_scores, decoder_input = torch.max(decoder_output, dim=1)
+            # Record token and score
+            all_tokens = torch.cat((all_tokens, decoder_input), dim=0)
+            all_scores = torch.cat((all_scores, decoder_scores), dim=0)
+            # Prepare current token to be next decoder input (add a dimension)
+            decoder_input = torch.unsqueeze(decoder_input, 0)
+        # Return collections of word tokens and scores
+        return [all_tokens], [-torch.sum(torch.log(all_scores).detach().cpu())/float(max_length - 1 + 1e-6)]
+    
+    
+import sentencepiece as spm
+segmentation_model_position = './data'
+segmentation_model_name = 'train_model32000.model'
+
+spp = spm.SentencePieceProcessor()
+spp.Load(os.path.join(segmentation_model_position, segmentation_model_name))
+def sentencePieceNormalizeString(input_sentence):
+    splitSentence = ' '.join(spp.EncodeAsPieces(input_sentence))
+    return splitSentence
+
+
+import MeCab
+tagger = MeCab.Tagger("-d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd/")
+
+def mecabNormalizeString(input_sentence):
+    splitSentence = ' '.join([ele.split("\t")[0] for ele in tagger.parse(input_sentence).split("\n")[:-2]])
+    return splitSentence
+
+
+
+
+class BeamSearchDecoder(nn.Module):
+    def __init__(self, encoder, decoder, beam_width, n_best):
+        super(BeamSearchDecoder, self).__init__()
+        
+        self.encoder = encoder
+        self.decoder = decoder
+        self.beam_width = beam_width
+        self.n_best = n_best
+        
+    def forward(self, input_seq, input_length, max_length):
+        # Forward input through encoder model
+        encoder_outputs, encoder_hidden = self.encoder(input_seq, input_length)
+        # Prepare encoder's final hidden layer to be first hidden input to the decoder
+        decoder_hidden = encoder_hidden[: self.decoder.n_layers]
+        # Initialize decoder input with SOS_token
+        #decoder_input = torch.ones(1, 1, device=device, dtype=torch.long) * SOS_token
+        
+        
+        class BeamSearchNode(object):
+            def __init__(self, hid, prevNode, wordId, logP, length):
+                self.hid = hid
+                self.prevNode = prevNode
+                self.wordId = wordId
+                self.logP = logP
+                self.length = length
+                
+            def eval(self, alpha=1.0):
+                reward = 0
+                # Add here a function for shaping a reward
+
+                return self.logP / float(self.length - 1 + 1e-6) + alpha * reward
+          
+        
+        n_best_batch_list = []
+        n_best_batch_score_list = []
+        for batchNum in range(encoder_hidden.size(1)):
+            
+            decoder_input = torch.LongTensor([[SOS_token]]).to(device)
+            # 一つ前の状態の隠れベクトル、単語をNodeを保持するNodeを生成
+            node = BeamSearchNode(hid=decoder_hidden[:,batchNum,:].unsqueeze(1), prevNode=None, wordId=decoder_input, logP=0, length=1)
+            nextNodes=[]
+            
+            nextNodes.append((-node.eval(), id(node), node))
+            n_dec_steps = 0
+            while True:
+                
+                nodes = [sorted(nextNodes)[inode] for inode in range(len(nextNodes)) if inode<self.beam_width]
+                nextNodes = []
+                end_node = []
+                for beamNum in range(self.beam_width):
+                    if len(nodes)<=0:
+                        break
+                    #今から探索するNodeを取得
+                    score, _, n = nodes.pop(0)
+                    decoder_input = n.wordId
+                    decoder_hidden = n.hid
+                    
+                    if n.wordId[0][0].item()!=EOS_token and n.wordId[0][0].item()!=PAD_token and n.length<=max_length:
+                        decoder_output, decoder_hidden = self.decoder(
+                            decoder_input, decoder_hidden, encoder_outputs
+                        )
+
+                        topk_prob, topk_indexes = torch.topk(decoder_output, self.beam_width) 
+
+                        for new_k in range(self.beam_width):
+                            decoded_t = topk_indexes[0][new_k].view(1,1) # (1)
+                            logp = torch.log(topk_prob[0][new_k]).item() # float log probability val
+
+                            node = BeamSearchNode(hid=decoder_hidden,
+                                                  prevNode=n,
+                                                  wordId=decoded_t,
+                                                  logP=n.logP+logp,
+                                                  length=n.length+1)
+                            nextNodes.append((-node.eval(), id(node), node))
+                            
+                            
+                    else:
+                        node = BeamSearchNode(hid=decoder_hidden,
+                                                  prevNode=n,
+                                                  wordId=torch.LongTensor([[PAD_token]]).to(device),
+                                                  logP=n.logP,
+                                                  length=n.length)
+                        nextNodes.append((-node.eval(), id(node), node))
+                        end_node.append(1)
+                if len(end_node)>=self.beam_width:
+                    break
+            
+            
+        
+            if len(nextNodes)!=self.beam_width:
+                print('assert not match beam width and nextNode length')
+                
+            n_best_seq_list = []
+            n_best_score_list = []
+            for score, _id, n in sorted(nextNodes):
+                sequence = [n.wordId.item()]
+                n_best_score_list.append(score)
+                # back trace from end node
+                while n.prevNode is not None:
+                    n = n.prevNode
+                    sequence.append(n.wordId.item())
+                    
+                sequence = sequence[::-1] # reverse
+                n_best_seq_list.append(sequence)
+        
+            n_best_seq_list = n_best_seq_list[::-1]
+            n_best_score_list = n_best_score_list[::-1]
+            
+            n_best_batch_list.append(n_best_seq_list)
+            n_best_batch_score_list.append(n_best_score_list)
+            
+        n_best_batch_list = torch.tensor(n_best_batch_list)
+        n_best_batch_score_list = torch.tensor(n_best_batch_score_list)
+            
+            #batchを無視して今回は出力することにした
+        return n_best_batch_list[0], n_best_batch_score_list[0]
+    
+
+
+def evaluate(encoder, decoder, searcher, voc, sentence, max_length=MAX_LENGTH, viewScore=False):
+    ### Format input sentence as a batch
+    # words -> indexes
+    indexes_batch = [indexesFromSentence(voc, sentence)]
+    # Create lengths tensor
+    lengths = torch.tensor([len(indexes) for indexes in indexes_batch])
+    # Transpose dimensions of batch to match models' expectations
+    input_batch = torch.LongTensor(indexes_batch).transpose(0, 1)
+    # Use appropriate device
+    input_batch = input_batch.to(device)
+    lengths = lengths.to(device)
+    # Decode sentence with searcher
+    seqs, scores = searcher(input_batch, lengths, max_length)
+    
+    for iseqs in range(len(seqs)):
+        tokens = seqs[iseqs]
+    
+        # indexes -> words
+        decoded_words = [voc.index2word[token.item()] for token in tokens]
+        if viewScore:
+            print(scores[iseqs],decoded_words)
+    return decoded_words
+
+
+def evaluateInput(encoder, decoder, searcher, searcher1, voc, mecab_or_sentencePiece, viewScore=True):
+    input_sentence = ""
+    while 1:
+        try:
+            # Get input sentence
+            input_sentence = input("> ")
+            # Check if it is quit case
+            if input_sentence == "q" or input_sentence == "quit":
+                break
+            # Normalize sentence
+            if mecab_or_sentencePiece=="mecab":
+                input_sentence = mecabNormalizeString(input_sentence)
+                
+            elif mecab_or_sentencePiece=="sentencePiece":
+                input_sentence = sentencePieceNormalizeString(input_sentence)
+                
+            print('\n入力を分割すると',input_sentence+'\n')
+            
+            # Evaluate sentence with seacher
+            output_words = evaluate(encoder, decoder, searcher, voc, input_sentence, viewScore=viewScore)
+            # Format and print response sentence
+            output_words[:] = [
+                x for x in output_words if not (x == "EOS" or x == "PAD" or x == "SOS")
+            ]
+            print("\nBot(BeamSearch):", " ".join(output_words))
+            
+            print()
+            
+            # Evaluate sentence with seacher1
+            output_words = evaluate(encoder, decoder, searcher1, voc, input_sentence, viewScore=viewScore)
+            # Format and print response sentence
+            output_words[:] = [
+                x for x in output_words if not (x == "EOS" or x == "PAD" or x == "SOS")
+            ]
+            print("\nBot(GreedySearch):", " ".join(output_words)+"\n")
+
+        except KeyError:
+            print("Error: Encountered unknown word.")
+
+
+
---
+++ model_lstm.py
@@ -0,0 +1,259 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import os
+
+
+
+
+PAD_token = 0  # Used for padding short sentences
+SOS_token = 1  # Start-of-sentence token
+EOS_token = 2  # End-of-sentence token
+
+
+import random
+import itertools
+
+
+def indexesFromSentence(voc, sentence):
+    return [voc.word2index[word] for word in sentence.split(" ")] + [EOS_token]
+
+
+def zeroPadding(l, fillvalue=PAD_token):
+    return list(itertools.zip_longest(*l, fillvalue=fillvalue))
+
+
+
+
+def binaryMatrix(l, value=PAD_token):
+    m = []
+    for i, seq in enumerate(l):
+        m.append([])
+        for token in seq:
+            if token == PAD_token:
+                m[i].append(0)
+            else:
+                m[i].append(1)
+    return m
+
+
+
+# Returns padded input sequence tensor and lengths
+def inputVar(l, voc):
+    indexes_batch = [indexesFromSentence(voc, sentence) for sentence in l]
+    lengths = torch.tensor([len(indexes) for indexes in indexes_batch])
+    padList = zeroPadding(indexes_batch)
+    padVar = torch.LongTensor(padList)
+    return padVar, lengths
+
+
+# Returns padded target sequence tensor, padding mask, and max target length
+def outputVar(l, voc):
+    indexes_batch = [indexesFromSentence(voc, sentence) for sentence in l]
+    max_target_len = max([len(indexes) for indexes in indexes_batch])
+    padList = zeroPadding(indexes_batch)
+    mask = binaryMatrix(padList)
+    mask = torch.BoolTensor(mask)  # Byte?
+    padVar = torch.LongTensor(padList)
+    return padVar, mask, max_target_len
+
+
+# Returns all items for a given batch of pairs
+def batch2TrainData(voc, pair_batch):
+    pair_batch.sort(key=lambda x: len(x[0].split(" ")), reverse=True)
+    input_batch, output_batch = [], []
+    for pair in pair_batch:
+        input_batch.append(pair[0])
+        output_batch.append(pair[1])
+    inp, lengths = inputVar(input_batch, voc)
+    output, mask, max_target_len = outputVar(output_batch, voc)
+    return inp, lengths, output, mask, max_target_len
+
+
+
+
+
+
+class EncoderRNN(nn.Module):
+    def __init__(self, hidden_size, embedding, n_layers=1, dropout=0):
+        super(EncoderRNN, self).__init__()
+        self.n_layers = n_layers
+        self.hidden_size = hidden_size
+        self.embedding = embedding
+        # Initialize GRU; the input_size and hidden_size params are both set to 'hidden_size'
+        #   because our input size is a word embedding with number of features == hidden_size
+#         self.gru = nn.GRU(
+#             hidden_size,
+#             hidden_size,
+#             n_layers,
+#             dropout=(0 if n_layers == 1 else dropout),
+#             bidirectional=True,
+#         )
+        self.lstm = nn.LSTM(
+            hidden_size,
+            hidden_size,
+            n_layers,
+            dropout=(0 if n_layers == 1 else dropout),
+            bidirectional=True,
+        )
+
+    def forward(self, input_seq, input_lengths, hidden=None):
+        # Convert word indexes to embeddings
+        embedded = self.embedding(input_seq)
+        # Pack padded batch of sequences for RNN module
+        packed = nn.utils.rnn.pack_padded_sequence(embedded, input_lengths)
+        # Forward pass through GRU
+        outputs, hidden = self.lstm(packed, hidden)
+
+        # Unpack padding
+        outputs, _ = nn.utils.rnn.pad_packed_sequence(outputs)
+        # Sum bidirectional GRU outputs
+        outputs = outputs[:, :, : self.hidden_size] + outputs[:, :, self.hidden_size :]
+        # [T x B x *]
+        # T is the length of the longest sequence
+        # B is the batch size
+        # * is any number of dimensions
+        
+        # hidden.size()=[(layer x numDirection) x B x *]
+        
+        # Return output and final hidden state
+        return outputs, hidden
+
+
+
+
+
+
+
+class Attn(nn.Module):
+    def __init__(self, method, hidden_size):
+        super(Attn, self).__init__()
+        self.method = method
+        self.hidden_size = hidden_size
+        if self.method not in ["dot", "general", "concat"]:
+            raise ValueError(self.method, "is not an appropriate attention method.")
+
+        elif self.method == "general":
+            self.attn = nn.Linear(self.hidden_size, self.hidden_size)
+
+        elif self.method == "concat":
+            self.attn = nn.Linear(self.hidden_size * 2, self.hidden_size)
+            self.v = nn.Parameter(torch.FloatTensor(hidden_size))
+            print('self.vのパラメタNaNの数',torch.sum(torch.isnan(self.v)))
+
+    def dot_score(self, hidden, encoder_outputs):
+        return torch.sum(encoder_outputs * hidden, dim=2)
+
+    def concat_score(self, hidden, encoder_outputs):
+        attn_energies = self.attn(
+            torch.cat(
+                (hidden.expand(encoder_outputs.size(0), -1, -1), encoder_outputs), dim=2
+            )
+        ).tanh()
+        
+        return torch.sum(attn_energies * self.v, dim=2)
+
+    def general_score(self, hidden, encoder_outputs):
+        energy = self.attn(encoder_outputs)
+        return torch.sum(energy * hidden, dim=2)
+
+    def forward(self, hidden, encoder_outputs):
+        # Calculate the attention weights (energies) based on the given method
+        if self.method == "general":
+            attn_energies = self.general_score(hidden, encoder_outputs)
+        elif self.method == "concat":
+            attn_energies = self.concat_score(hidden, encoder_outputs)
+
+        elif self.method == "dot":
+            attn_energies = self.dot_score(hidden, encoder_outputs)
+            
+        # Transpose max_length and batch_size dimensions
+        attn_energies = attn_energies.t()
+        # Return the softmax normalized probability scores (with added dimension)
+        return F.softmax(attn_energies, dim=1).unsqueeze(1)
+
+
+
+
+class AttnDecoderRNN(nn.Module):
+    def __init__(
+        self, attn_model, embeding, hidden_size, output_size, n_layers=1, dropout=0.1
+    ):
+        super(AttnDecoderRNN, self).__init__()
+
+        # Keep for reference
+        self.attn_model = attn_model
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.dropout = dropout
+
+        # Define layers
+        self.embedding = embeding
+        self.embedding_dropout = nn.Dropout(dropout)
+#         self.gru = nn.GRU(
+#             hidden_size,
+#             hidden_size,
+#             n_layers,
+#             dropout=(0 if n_layers == 1 else dropout),
+#         )
+        self.lstm = nn.LSTM(
+            hidden_size,
+            hidden_size,
+            n_layers,
+            dropout=(0 if n_layers == 1 else dropout),
+        )
+        self.concat = nn.Linear(hidden_size * 2, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+
+        self.attn = Attn(attn_model, hidden_size)
+
+    def forward(self, input_step, last_hidden, encoder_outputs):
+        # Note: we run this one step (word) at a time
+        # Get embedding of current input word
+        embedded = self.embedding(input_step)
+        embedded = self.embedding_dropout(embedded)
+        # Forward through unidirectional GRU
+        rnn_output, hidden = self.lstm(embedded, last_hidden)
+        # rnn_output.Size() = [1(sequenceLength) x B x *]
+        # hidden.Size() = [layer x B x *]
+        
+        # Calculate attention weights from the current GRU output
+        attn_weights = self.attn(rnn_output, encoder_outputs)
+        # Multiply attention weights to encoder outputs to get new "weighted sum" context vector
+        context = attn_weights.bmm(encoder_outputs.transpose(0, 1))
+        # Concatenate weighted context vector and GRU output using Luong eq. 5
+        rnn_output = rnn_output.squeeze(0)
+        context = context.squeeze(1)
+        concat_input = torch.cat((rnn_output, context), 1)
+        concat_output = torch.tanh(self.concat(concat_input))
+        # Predict next word using Luong eq. 6
+        output = self.out(concat_output)
+        output = F.softmax(output, dim=1)
+        # Return output and final hidden state
+        return output, hidden
+
+
+
+# パラメータカウント関数
+def parameters_count(net):
+    params = 0
+    for p in net.parameters():
+        #print(p)
+        #print(p.numel())
+        if p.requires_grad:
+            params += p.numel()
+        
+    print(params)
+
+
+
+
+
+
+
